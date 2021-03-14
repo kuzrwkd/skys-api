cacd8ca62030dccfb081f57b0f71fd0d9bb1d77e
増田 博紀 <kuzrwkd@icloud.com>
2021-03-14 20:47:18 +0900

    jestの環境整備

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps: [{
-    name: "investment-support-api",
-    script : "DEBUG=app:* npm start",
-    instances: 1,
-    autorestart: true,
-    restart_delay: 5000,
-    watch: ["dist"],
-    watch_delay: 1000,
-    ignore_watch : ["node_modules", "log-*.txt"],
-    watch_options: {
-      "followSymlinks": false
-    }
-  }]
+  apps: [
+    {
+      name: 'investment-support-api',
+      script: 'DEBUG=app:* npm start',
+      instances: 1,
+      autorestart: true,
+      restart_delay: 5000,
+      watch: ['app'],
+      watch_delay: 1000,
+      ignore_watch: ['node_modules', 'log-*.txt'],
+      watch_options: {
+        followSymlinks: false,
+      },
+    },
+  ],
 }

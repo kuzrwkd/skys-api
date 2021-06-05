820c147196c659e229f597f473811ab172463db3
増田 博紀 <kuzrwkd@icloud.com>
2021-06-05 22:44:56 +0900

    nestjsに変更する

--- app/calculation.ts
+++
@@ -1,4 +0,0 @@
-export const addition = (i: number, j: number): number => i + j
-export const subtraction = (i: number, j: number): number => i - j
-export const multiplication = (i: number, j: number): number => i * j
-export const division = (i: number, j: number): number => i / j
--- docker/mysql/initdb.d/create_db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `sample_db`;
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL ON sample_db.* TO 'user'@'%';

5a6d373c8a9e6233792aaf73f9aefaad26c21d38
Hiroki Masuda <kuzrwkd@icloud.com>
2023-07-01 17:16:18 +0900

    ♻️ refactoring

--- src/useCase/interact/newsFeedInteract.ts
+++ src/useCase/interact/newsFeedInteract.ts
@@ -17,18 +17,11 @@ export class NewsFeedInteract {
 
     result.forEach((item: NewsfeedSchema) => {
       const baseParams: APIResponseItem = {
-        id: item.id,
-        title: item.title,
-        url: item.url,
+        ...item,
         media: {
           media_id: null,
           name: mediaName,
         },
-        category: item.category,
-        article_created_at: item.article_created_at,
-        article_updated_at: item.article_updated_at,
-        created_at: item.created_at,
-        updated_at: item.updated_at,
       };
 
       res.push({ ...baseParams, media: { media_id: mediaId, name: mediaName } });
